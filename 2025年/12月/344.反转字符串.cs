efa87190687f6408f7955e22104cf0ec1dd79af1
BaiRong-NUC <gby@edu.nuc.email>
2025-12-09 14:41:48 +0800

    双指针

---
+++ 2025年/12月/344.反转字符串.cs
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=csharp
+ *
+ * [344] 反转字符串
+ */
+
+// @lc code=start
+public class Solution {
+    public void ReverseString(char[] s) {
+        int left = 0;
+        int right = s.Length - 1;
+        while (left < right)
+        {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
+// @lc code=end
+
