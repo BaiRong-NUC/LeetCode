c8058d4ea2b235cb1344946204683d107345a88c
BaiRong-NUC <gby@edu.nuc.email>
2025-12-10 21:42:48 +0800

    生成C#调试配置

---
+++ CSharpDebug/CSharpDebug.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharpDebug
+{
+    public class CSharpDebug
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Iteration: " + i);
+            }
+        }
+    }
+}
