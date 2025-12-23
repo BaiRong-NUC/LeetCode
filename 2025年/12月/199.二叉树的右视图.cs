57b41f95363450dddba0578a4d55e1ccdd397624
BaiRong-NUC <gby@edu.nuc.email>
2025-12-23 16:36:05 +0800

    二叉树的右视图为层序遍历的最后一个

---
+++ 2025年/12月/199.二叉树的右视图.cs
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=199 lang=csharp
+ *
+ * [199] 二叉树的右视图
+ */
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public IList<int> RightSideView(TreeNode root)
+    {
+        // 层序遍历的最后一个
+        var result = new List<int>();
+        if (root == null) return result;
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        while (queue.Count > 0)
+        {
+            int levelSize = queue.Count;
+            for (int i = 0; i < levelSize; i++)
+            {
+                var node = queue.Dequeue();
+                if (i == levelSize - 1)
+                {
+                    result.Add(node.val);
+                }
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
+
