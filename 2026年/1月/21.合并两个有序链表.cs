ed03b2bc13ccff490b4912896eb2f8171ebff183
BaiRong-NUC <gby@edu.nuc.email>
2026-01-30 19:41:21 +0800

    合并有序链表

---
+++ 2026年/1月/21.合并两个有序链表.cs
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=csharp
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode mergedHead = new ListNode(0);
+        ListNode current = mergedHead;
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val <= list2.val)
+            {
+                current.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        if (list1 != null)
+        {
+            current.next = list1;
+        }
+        if (list2 != null)
+        {
+            current.next = list2;
+        }
+        return mergedHead.next;
+    }
+}
+// @lc code=end
+
