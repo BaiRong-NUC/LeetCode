ec3c1601e26116c8b1ef021cda856446f87e5c01
BaiRong-NUC <gby@edu.nuc.email>
2026-01-30 19:37:59 +0800

    环形链表返回

---
+++ 2026年/1月/142.环形链表-ii.cs
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=csharp
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public ListNode DetectCycle(ListNode head)
+    {
+        if (head == null)
+        {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        bool hasCycle = false;
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+            {
+                hasCycle = true;
+                break;
+            }
+        }
+        if (!hasCycle)
+        {
+            return null;
+        }
+        slow = head;
+        while (slow != fast)
+        {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
+// @lc code=end
+
