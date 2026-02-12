10178f1983d9dea87f8465b4ec11b970f6023603
BaiRong-NUC <gby@edu.nuc.email>
2026-02-12 18:28:14 +0800

    将数组一分为二，其中一定有一个是有序的，另一个可能是有序，也能是部分有序。 此时有序部分用二分法查找。无序部分再一分为二，其中一个一定有序，另一个可能有序，可能无序。就这样循环.

---
+++ 2026年/2月/33.搜索旋转排序数组.cs
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=csharp
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+// @lc code=start
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int left = 0, right = nums.Length - 1;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            if (nums[0] <= nums[mid])
+            {
+                // 0...mid 有序
+                if (nums[0] <= target && target < nums[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            else
+            {
+                // mid...end 有序
+                if (nums[mid] < target && target <= nums[nums.Length - 1])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+// @lc code=end
+
